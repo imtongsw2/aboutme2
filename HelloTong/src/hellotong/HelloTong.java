f05b30c7af2ab22dd95eafad11004cee7996c753
ImTong <imtong.sword2@gmail.com>
2022-03-25 00:20:05 +0700

    Revert "cat and dog"
    This reverts commit 918960628f974eff11251256870167f9bbba1d53.

--- HelloTong/src/hellotong/HelloTong.java
+++ HelloTong/src/hellotong/HelloTong.java
@@ -15,7 +15,8 @@ public class HelloTong {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello Word 555");
+        System.out.println("Hello Word");
+        System.out.println("Love you");
     }
     
 }
